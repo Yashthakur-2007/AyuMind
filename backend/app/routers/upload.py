df70dbe81f45bf5e9d2be6077b702db477119900
Your Name <you@example.com>
2026-09-10 11:32:21 +0530

    text cleaning

---
+++ backend/app/routers/upload.py
@@ -0,0 +1,24 @@
+
+from app.services.text_cleaning_service import clean_text
+from fastapi import APIRouter, UploadFile, File
+from app.services.upload_service import save_uploaded_file
+from app.schemas.parser_schema import ParseRequest
+from app.services.pdf_parser_service import parse_pdf
+from app.services.chunking_service import chunk_text
+router = APIRouter()
+
+@router.post("/upload")
+def upload_file(file: UploadFile = File(...)):
+    file_path = save_uploaded_file(file)
+
+    extracted_text = parse_pdf(file_path)
+    cleaned_text = clean_text(extracted_text)
+    chunks = chunk_text(cleaned_text)
+
+    return {
+    "message": "File uploaded successfully",
+    "filename": file.filename,
+    "text": extracted_text,
+    "cleaned_text": cleaned_text,
+    "Chunks": chunks,
+}
